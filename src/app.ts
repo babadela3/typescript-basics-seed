e9f85f80586876dd189240400c74421f0177e0ae
teosposib <teodor.sposib@gmail.com>
2019-07-03 16:24:02 +0300

    Create Coupon class with static fields

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,9 @@
-abstract class Size {
-    constructor(protected size: string) {}
-
-    set availableSize(size:  string) {
-        this.size = size;
-    }
-
-    get availableSize(): string {
-        return this.size;
+class Coupon {
+    static nameArray = ['Coupon 25','Coupon 30'];
+    static create(pertenge: number) {
+        console.log(pertenge);
     }
 }
 
-class Pizza extends Size{
-    readonly name: string;
-    toppings: string[] = [];
-
-    constructor(name: string, size: string) {
-        super(size);
-        this.name = name;
-    }
-
-    public updateSize(size: string): void {
-        this.size = size;
-    }
-
-    addTopping(topping: string)  {
-        this.toppings.push(topping);
-    }
-}
-
-const pizza = new Pizza("Pepporoni", "small");
-pizza.addTopping("Bacon");
-pizza.addTopping("Cheese");
-pizza.updateSize("big");
-
-console.log(pizza);
-console.log(pizza.name);
-console.log(pizza.availableSize);
+console.log(Coupon.nameArray);
+console.log(Coupon.create(15));
