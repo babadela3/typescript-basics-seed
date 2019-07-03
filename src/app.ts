8d9cc375a033f8e14d34776972822fb50a7fe590
teosposib <teodor.sposib@gmail.com>
2019-07-03 12:43:56 +0300

    Use an interface to create a pizza

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello TypeScript!');
+interface Pizza {
+    name: string,
+    toppings: string[]
+}
+
+function createPizza(pizzaName: string, pizzaToppings: string[]): Pizza {
+    return {
+        name : pizzaName,
+        toppings : pizzaToppings
+    }
+}
+
+const pizza = createPizza("Pizza name", ["Topping 1", "Topping 2"]);
+
+console.log(pizza);
